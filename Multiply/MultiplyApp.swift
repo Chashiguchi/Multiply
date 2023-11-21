51e9102883bcd260c21350c7c81710c991f42da5
Chase Hashiguchi <chashiguchi@bsd220.org>
2023-11-21 09:25:53 -0600

    Initial Commit

---
+++ Multiply/MultiplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplyApp.swift
+//  Multiply
+//
+//  Created by chase Hashiguchi on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
